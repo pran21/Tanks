2ed56f4bf4ca620fb73c7dc9bd8d4a26fb8c0fc4
pran21 <praneeth.v.kanna@gmail.com>
2018-10-31 23:43:45 -0400

    fixed projectile motion

--- src/Projectile.java
+++ src/Projectile.java
@@ -21,7 +21,7 @@ public class Projectile {
 	
 	public void move(int xAmount, int yAmount) {
 		centerX += xAmount;
-		centerY += yAmount;
+		centerY -= yAmount;
 	}
 	
 	public void move() {
@@ -47,22 +47,36 @@ public class Projectile {
 	}
 	
 	public void setInMotion(int x, int y) {
-		int dX = x - centerX;
-		int dY = y - centerY;
+		double angle = 0;
+		double dX = Math.abs(x - centerX);
+		double dY = Math.abs(y - centerY);
+		double ratio = dY / dX; //trig ratio
+		
+		//System.out.println("dX: " + dX);
+		//System.out.println("dY: " + dY);
+		//System.out.println("ratio: " + ratio);
 		
 		//calculates distance between clicked point and position of projectile
 		//farther away the click, the higher the velocity
 		double distance = Math.sqrt((Math.pow(dX, 2) + Math.pow(dY, 2)));
 		int newVelocity = (int)distance / 75;
 		setVelocity(newVelocity);
-		System.out.println("New Velocity: " + newVelocity);
+		//System.out.println("New Velocity: " + newVelocity);
 		
 		//calculates angle between clicked point and position of projectile
 		//projectile moves directly opposite from click
-		double angle = Math.toDegrees(Math.atan(dY/dX));
-		int oppositeAngle = (int)angle + 180;
-		setDirection(oppositeAngle);
-		System.out.println("Launch Angle: " + oppositeAngle);
+		angle = Math.toDegrees(Math.atan(ratio));
+		if ((x > centerX) && (y < centerY)) {
+			angle = angle;  //first quadrant
+		} else if ((x < centerX) && (y < centerY)) {
+			angle = (180 - angle);  //second quadrant
+		} else if ((x < centerX) && (y > centerY)) {
+			angle = angle + 180;  //third quadrant
+		} else if ((x > centerX) && (y > centerY)) {
+			angle = (360 - angle);  //fourth quadrant
+		}
+		//System.out.println("angle: " + (int)angle);
+		setDirection((int) angle + 180);
 
 	}
 }
